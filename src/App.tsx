80532e8c573ab1620764129017b1d0010adc1875
Gulbaha <64098871+gulbaha19@users.noreply.github.com>
2022-06-21 17:21:41 +0600

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { TablePage } from "./pages/TablePage";
 
 function App() {
   return (
-    <Router>
+    <Router basename="build">
       <div className="App">
         <Routes>
           <Route path="/" element={<Main />} />
